45d8eff89add0cb822cc0b9d5cbf8eefd22a364e
jayasri <jayasri092@gmail.com>
2016-08-09 10:07:35 -0700

    added code

--- angulartask/app/services/api/students/students.service.js
+++ angulartask/app/services/api/students/students.service.js
@@ -6,26 +6,23 @@
 		.service('studentsService', StudentsService);
 
 	StudentsService.$inject = [];
-	function StudentsService() {
+	function StudentsService($scope,$http) {
 
-		/**
-		 * Exposed functions
-		 */
-
-	
-		var app = angular.module('myApp', []);
-    app.controller('myCtrl', function($scope, $http) {
-  $http.get("http://il-resume-api.azurewebsites.net/api/students").then(function (data1) {
-      $scope.myWelcome = data1.data;
-  });
-});
-
-		/**
-		 * Implementations
-		 */
-
+		$scope.StudentDetails=function(){
+    $http.get('http://il-resume-api.azurewebsites.net/api/students').then(function(value) {
+        $scope.Students = value.data;
+        $scope.status=value.status;
+if(value.status=="503"){
+  $scope.StudentDetails();
+        }
+    });
+    }
+    $scope.retrieveData=function(student){
+    $scope.StudentDetails=student;   
+    }
+}
 		function getName() {
 			return 'studentsService';
 		}
-	}
+	
 })();
